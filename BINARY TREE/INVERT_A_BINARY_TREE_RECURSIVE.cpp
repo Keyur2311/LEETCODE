bc0d3c349d1fa64db197cb35095c431f66df4272
KEYUR PATEL <82540200+Keyur2311@users.noreply.github.com>
2021-08-31 21:46:41 +0530

    Create INVERT_A_BINARY_TREE_RECURSIVE.cpp

---
+++ BINARY TREE/INVERT_A_BINARY_TREE_RECURSIVE.cpp
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution
+{
+public:
+    void reverseBinaryTree(TreeNode *&root)
+    {
+        if (root != NULL)
+        {
+            reverseBinaryTree(root->left);
+            reverseBinaryTree(root->right);
+            swap(root->left, root->right);
+        }
+    }
+    TreeNode *invertTree(TreeNode *root)
+    {
+        reverseBinaryTree(root);
+        return root;
+    }
+};
